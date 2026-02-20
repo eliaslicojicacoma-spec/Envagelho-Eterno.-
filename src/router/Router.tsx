0453a37ad8d822fb5443740b527140e12f5321ca
eliaslicojicacoma-spec <eliaslicojicacoma@gmail.com>
2026-02-20 06:58:48 +0100

    Update Router.tsx

--- src/router/Router.tsx
+++ src/router/Router.tsx
@@ -1,18 +1,10 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-import { Home } from "../pages/Home";
-import { Sobre } from "../pages/Sobre";
-import { Contacto } from "../pages/Contacto";
-import Biblia from "../pages/Biblia";
-
 export const Router = () => {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/sobre" element={<Sobre />} />
-        <Route path="/contacto" element={<Contacto />} />
-        <Route path="/biblia" element={<Biblia />} />
+        <Route path="/" element={<div>Home</div>} />
       </Routes>
     </BrowserRouter>
   );
