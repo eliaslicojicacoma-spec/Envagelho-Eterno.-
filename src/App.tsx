4f33c50e59ff4c738a45e4aaf21fc160c3205032
eliaslicojicacoma-spec <eliaslicojicacoma@gmail.com>
2026-02-20 07:00:26 +0100

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,7 @@
-import { BrowserRouter } from 'react-router-dom';
-import { Header } from './components/layout/Header';
-import { Footer } from './components/layout/Footer';
-import { Router } from './router/Router';
-import './i18n';
+import { Router } from "./router/Router";
 
-export default function App() {
-  return (
-    <BrowserRouter>
-      <div className="min-h-screen flex flex-col">
-        <Header />
-        <main className="flex-grow">
-          <Router />
-        </main>
-        <Footer />
-      </div>
-    </BrowserRouter>
-  );
+function App() {
+  return <Router />;
 }
+
+export default App;
