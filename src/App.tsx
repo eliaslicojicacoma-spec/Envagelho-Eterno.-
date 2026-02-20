c2636e8762ad84c23889393a3ca9f6f6b32463bd
eliaslicojicacoma-spec <eliaslicojicacoma@gmail.com>
2026-02-20 07:21:09 +0100

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,25 @@
-import { Router } from "./router/Router";
+import React from 'react';
+import { Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import Sobre from './pages/Sobre';
+import Biblia from './pages/Biblia';
+import Contacto from './pages/Contacto';
+import Header from './components/layout/Header';
+import Footer from './components/layout/Footer';
 
-function App() {
-  return <Router />;
-}
+const App: React.FC = () => {
+  return (
+    <>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/sobre" element={<Sobre />} />
+        <Route path="/biblia" element={<Biblia />} />
+        <Route path="/contacto" element={<Contacto />} />
+      </Routes>
+      <Footer />
+    </>
+  );
+};
 
 export default App;
