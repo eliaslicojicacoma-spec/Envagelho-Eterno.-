b4d4686619b16bd973b4a5e38846c395b9736a98
eliaslicojicacoma-spec <eliaslicojicacoma@gmail.com>
2026-02-20 07:35:21 +0100

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -2,23 +2,17 @@ import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import Sobre from './pages/Sobre';
-import Biblia from './pages/Biblia';
 import Contacto from './pages/Contacto';
-import Header from './components/layout/Header';
-import Footer from './components/layout/Footer';
+import Biblia from './pages/Biblia';
 
 const App: React.FC = () => {
   return (
-    <>
-      <Header />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/sobre" element={<Sobre />} />
-        <Route path="/biblia" element={<Biblia />} />
-        <Route path="/contacto" element={<Contacto />} />
-      </Routes>
-      <Footer />
-    </>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/sobre" element={<Sobre />} />
+      <Route path="/contacto" element={<Contacto />} />
+      <Route path="/biblia" element={<Biblia />} />
+    </Routes>
   );
 };
 
